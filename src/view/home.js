e1b1022a7c26ffdefb763398628acceec78fb96b
noemimatias <noemi.071213@gmail.com>
2019-07-09 18:29:59 -0500

    creando template de primera vista... Fixed #5

--- src/view/home.js
+++ src/view/home.js
@@ -1,2 +1,15 @@
 
-export const
+export const viewHome = () => {
+    const homeTemplate = `
+    <header>  
+    </header>
+    <h1>Team Burguer Queen</h1>
+    <button id="waiter" type="button">Meserx</button>
+    <button id="kitchen" type="button">Cocinerx</button>
+
+    `
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = homeTemplate;
+
+    return createDiv;
+}
