aef51478262bbb2415aae9c517eee7b4f10601fa
dajhelyan <assiaxramx10@gmail.com>
2019-07-17 12:05:22 -0500

    almacenando en un array productos seleccionados y pintando en pantalla la lista de productos seleccionados... Fixed #8

--- src/view/view-waiter/view-desayuno.js
+++ src/view/view-waiter/view-desayuno.js
@@ -1,27 +1,31 @@
 
+const arrOrders = [];
+
 const productElement = (product) => {
     const tmpl = `
     <p>${product.producto}</p>
     <p>${product.precio}<p>
     <button type="button" id="btn-add-${product.id}">Añadir</button>
     `
-    /* const tmplListAdd = `
+    const tmplListAdd = `
         <p>${product.producto}<p>
         <p>${product.precio}</p>
         <button>Eliminar</button>  
-    ` */
+    `
 
     const divSingleProduct = document.createElement('div');
     divSingleProduct.innerHTML = tmpl;
-/* 
+
     const liAddProduct = document.createElement('li');
-    liAddProduct.innerHTML = tmplListAdd; */
+    liAddProduct.innerHTML = tmplListAdd;
 
-    /* const listOrder = document.getElementById('see-order'); */
+    const listOrder = document.getElementById('see-order');
 
-    /* divSingleProduct.querySelector('button').addEventListener('click', () => {
+    divSingleProduct.querySelector('button').addEventListener('click', () => {
         listOrder.appendChild(liAddProduct)
-    }) */
+        console.log(arrOrders)
+        return arrOrders.push(product)
+    })
 
     return divSingleProduct;
 }
@@ -47,15 +51,7 @@ export const showLunch = (callback) => {
     callback()
         .then((result) => {
             result.forEach(product => {
-                console.log(result, 'e')
-                container.innerHTML += `
-            <div class="single-product-lunch">
-                <img src="">
-                <p>${product.producto}</p>
-                <p>${product.precio}<p>
-                <button type="button">Añadir</button>
-            </div> 
-            `
+                container.appendChild(productElement(product));
                 /* console.log(product.producto,'7') */
             });
         });
